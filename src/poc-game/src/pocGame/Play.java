551d68c70c9a71b92e54ebda97ee80da252e60e3
Everton Ferreira <everton.ferreira@hotmail.com>
2019-04-25 18:26:37 -0300

    menu and play class

---
+++ src/poc-game/src/pocGame/Play.java
@@ -0,0 +1,28 @@
+package pocGame;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Play extends BasicGameState{
+	
+		public Play(int state) {
+			
+		}
+		@Override
+		public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+			
+		}
+		@Override
+		public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+			
+		}
+		@Override
+		public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+			
+			
+		}
+		@Override
+		public int getID() {
+			return 1;
+		}
+}
