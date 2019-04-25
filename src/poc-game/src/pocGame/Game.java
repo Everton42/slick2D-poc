8411be248c9c899566013974d051f0c460cde223
Everton Ferreira <everton.ferreira@hotmail.com>
2019-04-25 18:11:52 -0300

    states

--- src/poc-game/src/pocGame/Game.java
+++ src/poc-game/src/pocGame/Game.java
@@ -1,10 +1,33 @@
 package pocGame;
 
-public class Game {
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Game extends StateBasedGame{
 
+	public static final String gameName = "Poc Game!";
+	public static final int menu = 0;
+	public static final int play = 1;
+	
+	public Game(String name) {
+		super(gameName);
+		this.addState(new Menu(menu));
+		this.addState(new Play(play));
 	}
+	
+	
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(menu).init(gc, this);
+		this.getState(play).init(gc, this);
+		this.enterState(menu);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+
+
 
 }
