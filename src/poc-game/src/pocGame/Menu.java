7333402476ed3edd02c1c2d88615cc05656195fa
Everton Ferreira <everton.ferreira@hotmail.com>
2019-04-25 22:03:23 -0300

    draw images and stuffs

--- src/poc-game/src/pocGame/Menu.java
+++ src/poc-game/src/pocGame/Menu.java
@@ -6,7 +6,7 @@ import org.newdawn.slick.state.*;
 public class Menu extends BasicGameState{
 	
 		public Menu(int state) {
-			
+		
 		}
 		@Override
 		public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
@@ -14,7 +14,11 @@ public class Menu extends BasicGameState{
 		}
 		@Override
 		public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-			
+			g.drawString("Don't Panic!", 50, 50);
+			//g.drawRect(50, 100, 60, 120);
+			//g.drawOval(200, 130, 130, 80);
+			Image marvin = new Image("res/marvin.png");
+			g.drawImage(marvin, 100, 100);
 		}
 		@Override
 		public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
